bd47f6f27b53af1342bc58335627c0a53e1cc07a
Austin Baxley <ausbaxley@gmail.com>
2023-04-27 13:34:52 -0400

    edited projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MarketFinder from "../assets/MarketFinder.png";
 import GetMoneySmart from "../assets/GetMoneySmart.png";
+import WorkoutTracker from "../assets/Workout.png";
 
 const Work = () => {
   return (
@@ -25,12 +26,20 @@ const Work = () => {
                 Market Finder
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://abaxley2.github.io/Market-Finder/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a
+                  href="https://github.com/abaxley2/Market-Finder"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -39,125 +48,9 @@ const Work = () => {
             </div>
           </div>
           {/* Grid item 2 */}
-          <div
-            style={{ backgroundImage: `url(${MarketFinder})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl fond-bold text-[#ccd6f6] tracking-wider">
-                Market Finder
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item 3 */}
-          <div
-            style={{ backgroundImage: `url(${MarketFinder})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl fond-bold text-[#ccd6f6] tracking-wider">
-                Market Finder
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item 4 */}
-          <div
-            style={{ backgroundImage: `url(${MarketFinder})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl fond-bold text-[#ccd6f6] tracking-wider">
-                Market Finder
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item 5 */}
-          <div
-            style={{ backgroundImage: `url(${MarketFinder})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl fond-bold text-[#ccd6f6] tracking-wider">
-                Market Finder
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item 6 */}
-          <div
-            style={{ backgroundImage: `url(${MarketFinder})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl fond-bold text-[#ccd6f6] tracking-wider">
-                Market Finder
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+          {/* Grid item 3 */}
+          {/* Grid item 4 */}
+          {/* Grid item 5 */}
         </div>
       </div>
     </div>
