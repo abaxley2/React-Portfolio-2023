4e88e3af9fd93df0792bc811228ec2b059bc1c2b
Austin Baxley <ausbaxley@gmail.com>
2023-05-07 15:59:14 -0400

    added coments to app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+// importing the necessary components
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Skills from "./components/Skills";
-import Work from "./components/Work";
+// import Work from "./components/Work";
 
+// defining the main app component
 function App() {
   return (
     <div>
@@ -18,4 +20,5 @@ function App() {
   );
 }
 
+// exporting the app component
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+// Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Import the index.css file
 import "./index.css";
+
+// Import the App component
 import App from "./App";
 
+// Create a new React root with the HTML element that has an ID of "root"
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component wrapped in React.StrictMode to catch potential issues
 root.render(
   <React.StrictMode>
     <App />
